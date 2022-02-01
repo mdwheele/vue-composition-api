4d3c450925750f92c3d0d2c98da6e48161d01fe3
Dustin Wheeler <dustin.wheeler@nutanix.com>
2022-01-31 22:52:24 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="flex flex-col items-center justify-center h-screen font-sans bg-gradient-to-r from-purple-500 to-indigo-500">
+    <div class="flex space-x-8">
+      <div class="p-6 overflow-hidden text-center bg-white shadow-xl rounded-2xl">
+        <Identicon :seed="normalizedInput" class="w-32 h-32" />
+      </div>
+      <div class="p-6 overflow-hidden text-center bg-white shadow-xl rounded-2xl">
+        <Identicon theme="retro" :seed="normalizedInput" class="w-32 h-32" />
+      </div>
+    </div>
+
+    <div class="my-8">
+      <GitHubProfile :username="input" />
+    </div>
+
+    <div class="w-96">
+      <input type="text" v-model="input" class="w-full rounded-2xl p-6 text-2xl shadow-xl focus:outline-none" placeholder="Type something..." />
+    </div>
+  </div>
+</template>
+
+<script>
+import Identicon from 'vue-identicon'
+import GitHubProfile from '@/components/GitHubProfile.vue'
+
+export default {
+  components: { Identicon, GitHubProfile },
+
+  data() {
+    return {
+      input: 'mdwheele'
+    }
+  },
+
+  computed: {
+    normalizedInput() {
+      return `${this.input}${this.input}`
+    }
+  }
+}
+</script>
---
+++ src/components/GitHubProfile.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="rounded-2xl p-6 bg-white shadow-xl">
+    <div v-if="loading">
+      <Icon name="refresh" outline class="boxw-16 h-16 shrink-0 animate-spin text-gray-500" />
+    </div>
+    <div v-else class="flex items-start space-x-6">
+      <img :src="user.avatar" class="w-32 h-32 rounded-full"/>
+      <div>
+        <div class="font-semibold text-2xl text-gray-800">{{ user.name }}</div>
+        <div class="text-gray-500">{{ user.username }}</div>
+        <div class="mt-2 text-sm space-y-0.5">
+          <div class="flex items-center space-x-1.5">
+            <Icon name="office-building" outline class="w-5 h-5 shrink-0 text-gray-600" />
+            <span>{{ user.company }}</span>
+          </div>
+          <div class="flex items-center space-x-1.5">
+            <Icon name="location-marker" outline class="w-5 h-5 shrink-0 text-gray-600" />
+            <span>{{ user.location }}</span>
+          </div>
+          <div class="flex items-center space-x-1.5">
+            <Icon name="cake" outline class="w-5 h-5 shrink-0 text-gray-600" />
+            <span>{{ user.twitter }}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Icon from 'vue-heroicon-next'
+
+export default {
+  name: 'GitHubProfile',
+
+  components: { Icon },
+
+  props: {
+    username: { 
+      type: String,
+      required: true
+    }
+  },
+
+  watch: {
+    username() {
+      if (this._debounce) {
+        clearTimeout(this._debounce)
+      }
+      
+      this._debounce = setTimeout(() => {
+        this.fetchUser()
+      }, 600)
+    }
+  },
+
+  data() {
+    return {
+      loading: false,
+      user: {}
+    }
+  },
+
+  methods: {
+    fetchUser() {      
+      this.loading = true
+
+      fetch(`https://api.github.com/users/${this.username}`)
+        .then(response => response.json())
+        .then(user => {
+          this.user = {
+            name: user.name,
+            username: user.login,
+            avatar: user.avatar_url,
+            company: user.company,
+            location: user.location,
+            twitter: user.twitter_username
+          }
+        })
+        .finally(() => {
+          this.loading = false
+
+          this._usernameDebounce = undefined
+        })
+    }
+  },
+
+  created() {
+    this.fetchUser()
+  }
+}
+</script>
