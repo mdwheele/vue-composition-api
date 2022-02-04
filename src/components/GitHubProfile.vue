5ce13e7ec5bd37336c885e0729d3801af8be1cfd
Dustin Wheeler <dustin.wheeler@nutanix.com>
2022-02-04 10:58:30 -0500

    Add hireable/productive checkmark

--- src/components/GitHubProfile.vue
+++ src/components/GitHubProfile.vue
@@ -4,7 +4,12 @@
       <Icon name="refresh" outline class="boxw-16 h-16 shrink-0 animate-spin text-gray-500" />
     </div>
     <div v-else class="flex items-start space-x-6">
-      <img :src="user.avatar" class="w-32 h-32 rounded-full"/>
+      <div class="relative">
+        <img :src="user.avatar" class="w-32 h-32 rounded-full"/>
+        <div v-if="isHireableAndProductive" class="absolute right-0 bottom-0 bg-green-600 rounded-full p-1.5 ring-4 ring-white">
+          <Icon name="check" class="w-5 h-5 text-white" />
+        </div>
+      </div>
       <div>
         <div class="font-semibold text-2xl text-gray-800">{{ user.name }}</div>
         <div class="text-gray-500">{{ user.username }}</div>
@@ -61,6 +66,12 @@ export default {
     }
   },
 
+  computed: {
+    isHireableAndProductive() {
+      return this.user.hireable === true && this.user.repos >= 10
+    }
+  },
+
   methods: {
     fetchUser() {      
       this.loading = true
@@ -70,14 +81,16 @@ export default {
 
       fetch(`https://api.github.com/users/${this.username}`, { headers })
         .then(response => response.json())
-        .then(user => {
+        .then(response => {
           this.user = {
-            name: user.name,
-            username: user.login,
-            avatar: user.avatar_url,
-            company: user.company,
-            location: user.location,
-            twitter: user.twitter_username
+            name: response.name,
+            username: response.login,
+            avatar: response.avatar_url,
+            company: response.company,
+            location: response.location,
+            twitter: response.twitter_username,
+            hireable: response.hireable,
+            repos: response.public_repos
           }
         })
         .finally(() => {
